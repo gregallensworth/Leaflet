608e8bf9133fb51dc88db7c0f7df3485df049972
gregallensworth <greg.allensworth@gmail.com>
2014-02-27 17:59:10 -0800

    Update MultiPolyLine_Distance.js
    Added the Polyline versions of these functions

--- MultiPolyLine_Distance.js
+++ MultiPolyLine_Distance.js
@@ -1,15 +1,18 @@
 /*
- * Add some methods to Leaflet MultiPolyline, for finding the minimm distance to a given LatLng.
+ * Add some methods to Leaflet Polyline and MultiPolyline, for finding the minimum distance to a given LatLng.
  * Useful for a quick-and-dirty "Is this line within X miles?" search.
  * Not precise as it sticks to vertices rather than actual shortest distance,
  * see "Gregor the Map Guy" blog for discussion on the shortcomings.
+ * 
+ * Also provided is simpler/faster hasVertexWithinRadius() function.
  * 
  * Greg Allensworth   <greg.allensworth@gmail.com>
  * No license, use as you will, kudos welcome but not required, etc.
  */
 
 
-// loop over vertices
+// loop over vertices, find the closest vertex to the given LatLng
+// return both the LatLng and the distance (meters)
 L.MultiPolyline.prototype.calculateClosestVertexAndDistance = function (targetlatlng) {
     var closest_latlng = null;
     var closest_meters = 1000000000;
@@ -28,6 +31,20 @@ L.MultiPolyline.prototype.calculateClosestVertexAndDistance = function (targetla
 
     return { latlng:closest_latlng, meters:closest_meters };
 }
+L.Polyline.prototype.calculateClosestVertexAndDistance = function (targetlatlng) {
+    var closest_latlng = null;
+    var closest_meters = 1000000000;
+
+    var verts = this.getLatLngs();
+    for (var vi=0, vl=verts.length; vi<vl; vi++) {
+        var d = verts[vi].distanceTo(targetlatlng);
+        if (d >= closest_meters) continue;
+        closest_latlng = path[vi];
+        closest_meters = d;
+    }
+
+    return { latlng:closest_latlng, meters:closest_meters };
+}
 
 
 // wrappers for calculateClosestVertexAndDistance() to return only the distance or the LatLng instance
@@ -37,6 +54,14 @@ L.MultiPolyline.prototype.closestVertexTo = function (latlng) {
     return closest.latlng;
 }
 L.MultiPolyline.prototype.shortestDistanceTo = function (latlng) {
+    var closest = this.calculateClosestVertexAndDistance(latlng);
+    return closest.meters;
+}
+L.Polyline.prototype.closestVertexTo = function (latlng) {
+    var closest = this.calculateClosestVertexAndDistance(latlng);
+    return closest.latlng;
+}
+L.Polyline.prototype.shortestDistanceTo = function (latlng) {
     var closest = this.calculateClosestVertexAndDistance(latlng);
     return closest.meters;
 }
@@ -53,6 +78,15 @@ L.MultiPolyline.prototype.hasVertexWithinRadius = function (targetlatlng,meters)
             var d = path[vi].distanceTo(targetlatlng);
             if (d <= meters) return true;
         }
+    }
+
+    return false;
+}
+L.Polyline.prototype.hasVertexWithinRadius = function (targetlatlng,meters) {
+    var vertss = this.getLatLngs();
+    for (var vi=0, vl=verts.length; vi<vl; vi++) {
+        var d = verts[vi].distanceTo(targetlatlng);
+        if (d <= meters) return true;
     }
 
     return false;
